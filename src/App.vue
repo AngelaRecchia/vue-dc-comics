e5744968b21e3783414a1a9ec571ed3ea688ca0d
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-07-06 14:46:18 +0200

    init components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
+/* Create un nuovo progetto utilizzando Vue CLI e definite i componenti
+necessari per strutturare il layout come da screenshot allegato. Quando la
+struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando
+i data del componente. Per oggi diamo priorità alla struttura: quando è tutto
+bello solido, passiamo al Sass! Bonus: Creare un componente aggiuntivo per
+gestire la fascia azzurra con le icone. */
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import Content from "./components/Content.vue";
+import Bluenav from "./components/Bluenav.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
+    Content,
+    Bluenav,
+    Footer,
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
