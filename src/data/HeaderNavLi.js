504f7721eb97e26582a7971236eb4d7682e25ec4
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-07-07 14:45:18 +0200

    header nav import

---
+++ src/data/HeaderNavLi.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        text: "Characters",
+        link: "#",
+        current: false
+    },
+    {
+        text: "Comics",
+        link: "#",
+        current: true
+    },
+    {
+        text: "Movies",
+        link: "#",
+        current: false
+    },
+    {
+        text: "Tv",
+        link: "#",
+        current: false
+    },
+    {
+        text: "Games",
+        link: "#",
+        current: false
+    },
+    {
+        text: "Collectibles",
+        link: "#",
+        current: false
+    },
+    {
+        text: "Videos",
+        link: "#",
+        current: false
+    },
+    {
+        text: "Fans",
+        link: "#",
+        current: false
+    },
+    {
+        text: "News",
+        link: "#",
+        current: false
+    },
+    {
+        text: "Shop",
+        link: "#",
+        current: false
+    }
+]
